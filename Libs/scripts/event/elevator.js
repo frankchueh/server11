8975595c3719526d111113d7ffd56f8a5a68c90c
Frank Chueh <as8307044@gmail.com>
2017-06-11 02:12:21 +0800

    Fix elevator dest

--- Libs/scripts/event/elevator.js
+++ Libs/scripts/event/elevator.js
@@ -7,33 +7,36 @@ function scheduleNew() {
     em.setProperty("isDown","false");
     onDown();
 }
-
+// start transfer down
 function onDown() {
-    em.getChannelServer().getMapFactory().getMap(222020100).resetReactors();
     em.warpAllPlayer(222020210, 222020211);
-    em.setProperty("isDown","true");
+    em.setProperty("isDown","false");
+    em.getChannelServer().getMapFactory().getMap(222020100).resetReactors();
     em.schedule("goingUp", 60000);
 }
 
+// done transfer down and wait for user going up
 function goingUp() {
-    em.warpAllPlayer(222020110, 222020111);
-    em.setProperty("isDown","false");
-    em.schedule("onUp", 50000);
+    em.warpAllPlayer(222020111, 222020200);
+    em.setProperty("isUp","true");
     em.getChannelServer().getMapFactory().getMap(222020100).setReactorState();
+    em.schedule("onUp", 50000);
 }
 
+// start transfer up
 function onUp() {
+    em.warpAllPlayer(222020110, 222020111);
+    em.setProperty("isUp","false");
     em.getChannelServer().getMapFactory().getMap(222020200).resetReactors();
-    em.warpAllPlayer(222020111, 222020100);
-    em.setProperty("isUp","true");
     em.schedule("goingDown", 60000);
 }
 
+// done transfer up and wait for user going down
 function goingDown() {
     em.warpAllPlayer(222020211, 222020100);
-    em.setProperty("isUp","false");
-    em.schedule("onDown", 50000);
+    em.setProperty("isDown","true");
     em.getChannelServer().getMapFactory().getMap(222020200).setReactorState();
+    em.schedule("onDown", 50000);
 }
 
 function cancelSchedule() {
